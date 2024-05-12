4beaf40771e75b975a517b0d6d8ae14774a45926
MootSeeker <mootseeker98@gmail.com>
2024-05-12 16:32:05 +0200

    extended base class

--- Core/Lib/base.cpp
+++ Core/Lib/base.cpp
@@ -5,31 +5,75 @@
  *      Author: kevin
  */
 
-
 #include "base.h"
 
 // Constructor
 bIO::bIO() {}
+
 // Destructor
 bIO::~bIO() {}
 
-
+/**
+ * @brief General read function.
+ * @return The read value.
+ */
 unsigned bIO::read( void )
 {
-	return 0;
+    return 0; // Placeholder return value
 }
 
+/**
+ * @brief General write function.
+ * @param data The data to be written.
+ */
 void bIO::write( unsigned data )
 {
-	__UNUSED( data );
+    __UNUSED( data ); // Unused parameter
 }
 
+/**
+ * @brief Stream data.
+ * @param buffer Pointer to the data buffer.
+ * @param length Length of the buffer.
+ */
 void bIO::stream( volatile void* buffer, int length )
 {
-	__UNUSED( buffer );
-	__UNUSED( length );
+    __UNUSED( buffer ); // Unused parameter
+    __UNUSED( length ); // Unused parameter
 }
 
+/**
+ * @brief Initialize the peripheral.
+ * This function will be overwritten by subclass.
+ */
+void bIO::init( void )
+{
+    // Do nothing
+}
 
+/**
+ * @brief Reset the peripheral.
+ * This function will be overwritten by subclass.
+ */
+void bIO_reset( void )
+{
+    // Do nothing
+}
 
+/**
+ * @brief Disable the peripheral.
+ * This function will be overwritten by subclass.
+ */
+void bIO::disable( void )
+{
+    // Do nothing
+}
 
+/**
+ * @brief Get the status of the peripheral.
+ * @return The status of the peripheral.
+ */
+HAL_StatusTypeDef bIO::get_status( void )
+{
+    return this->status; // Return the status
+}
--- Core/Lib/base.h
+++ Core/Lib/base.h
@@ -18,40 +18,68 @@ extern "C" {
 }
 #endif
 
-
-
-
-// Class declaration for basic input / output structures
-// Usable for GPIO, USART, SPI, I2C, etc.
+/**
+ * @brief Class declaration for basic input/output structures.
+ *
+ * This class can be used for GPIO, USART, SPI, I2C, etc.
+ */
 class bIO
 {
-	public:
-		bIO( );
-		~bIO( );
+    public:
+        /**
+         * @brief Constructor.
+         */
+        bIO( );
 
-		// General read and write functions
-		virtual unsigned read( void );
-		virtual void write( unsigned dat );
-		virtual void stream( volatile void *buf, int len );
+        /**
+         * @brief Destructor.
+         */
+        ~bIO( );
 
-		// Common peripheral control methods.
-		virtual void clock_en( void );
-		virtual void reset( void );
-		virtual void disable( void );
-		virtual int get_status( void );
-	protected:
+        /**
+         * @brief General read function.
+         * @return The read value.
+         */
+        virtual unsigned read( void );
 
-		HAL_StatusTypeDef status = HAL_ERROR;
+        /**
+         * @brief General write function.
+         * @param data The data to be written.
+         */
+        virtual void write( unsigned data );
 
-		// Enable/disable/reset register definitions.
-		__IO uint32_t *enable_reg = 0;
-		__IO uint32_t *reset_reg = 0;
-		uint32_t enable_bit = 0;
-		uint32_t reset_bit = 0;
+        /**
+         * @brief Stream data.
+         * @param buffer Pointer to the data buffer.
+         * @param length Length of the buffer.
+         */
+        virtual void stream( volatile void *buffer, int length );
 
-	private:
+        /**
+         * @brief Initialize the peripheral.
+         */
+        virtual void init( void );
 
+        /**
+         * @brief Reset the peripheral.
+         */
+        virtual void reset( void );
+
+        /**
+         * @brief Disable the peripheral.
+         */
+        virtual void disable( void );
+
+        /**
+         * @brief Get the status of the peripheral.
+         * @return The status of the peripheral.
+         */
+        virtual HAL_StatusTypeDef get_status( void );
+
+    protected:
+        HAL_StatusTypeDef status = HAL_ERROR; /**< Status of the peripheral. */
+
+    private:
 };
-
 
 #endif /* LIB_BASE_H_ */
